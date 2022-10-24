24f6ac8568676416c760b97b8cc667f3f88e5c1c
Vikhyat01 <83632491+Vikhyat01@users.noreply.github.com>
2022-10-24 22:17:03 +0530

    Update gforms.py

--- gforms.py
+++ gforms.py
@@ -1,23 +1,29 @@
 import csv
 
 
-def createurl(line):
-  URL = "https://docs.google.com/forms/d/e/1FAIpQLSdvCcqTyMU2XAeySFJjgcVhDTCEI3PBmhWs3FdhzHAGhOZgEQ/viewform?usp=pp_url&" + "entry.883835826=" + line[
-    1] + "&entry.1784547995=" + line[2] + "&entry.16777235=" + line[3]
+def createurl(P1, P2, P3):
+  URL = "https://docs.google.com/forms/d/e/1FAIpQLSdvCcqTyMU2XAeySFJjgcVhDTCEI3PBmhWs3FdhzHAGhOZgEQ/viewform?usp=pp_url&" + "entry.883835826=" + P1 + "&entry.1784547995=" + P2 + "&entry.16777235=" + P3
   return URL
 
 
 def Createdoc(name):
   f = open(name, encoding='UTF8')
   csv_reader = csv.reader(f)
+  #skipping header(first) line
   next(csv_reader)
   file = open("data.txt", "w")
   for line in csv_reader:
-    file.write("email  --subject='some string' --to=" + line[0] +
-               " << EOM\n\n")
-    file.write("For P1 = " + line[1] + ", P2= " + line[2] + " and P3 = " +
-               line[3] + "\n")
-    file.write("Please fill out [this form] (" + createurl(line) + ")\n\n")
+    #splitting up every line in csv into separate variables
+    Email = line[0]
+    P1val = line[1]
+    P2val = line[2]
+    P3val = line[3]
+    file.write("email  --subject='some string' --to=" + Email + " << 
+    EOM\n\n")
+    file.write("For P1 = " + P1val + ", P2= " + P2val + " and P3 = " + 
+    P3val    + "\n")
+    file.write("Please fill out [this form] (" +
+               createurl(P1val, P2val, P3val) + ")\n\n")
     file.write("EOM\n\n")
   file.close()
 
